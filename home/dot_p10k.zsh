11af1c9d3b40f40b2839847d296903943e475ef5
Zafar Mahmood <zafar@zafarmahmood.com>
2026-09-17 10:25:22 -0400

    chore(dotfiles): drop the now-unneeded noka suppression on p10k config
    home/dot_p10k.zsh is excluded from the zshellcheck hook as of f44538f,
    so the in-file ZC1075 suppression this comment carried is dead.

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -194,5 +194,5 @@
 # Tell `p10k configure` which file it should overwrite.
 typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
-((${#p10k_config_opts})) && setopt ${p10k_config_opts[@]} # noka: ZC1075
+((${#p10k_config_opts})) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
